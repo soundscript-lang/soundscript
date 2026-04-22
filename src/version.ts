4e3c6b3dcaa70941fd321f02d3e43abe55261f7b
Jake McCloskey <jakemccloskey@gmail.com>
2026-04-22 09:41:44 -0400

    Bump Soundscript version to 0.1.37

--- src/version.ts
+++ src/version.ts
@@ -1,4 +1,4 @@
-export const VERSION = '0.1.36';
+export const VERSION = '0.1.37';
 
 export function getSoundscriptToolFingerprint(): string {
   const override = Deno.env.get('SOUNDSCRIPT_CACHE_TOOL_FINGERPRINT');
